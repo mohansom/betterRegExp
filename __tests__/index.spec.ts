76193db52b2d6ea237deb4a55b20e0d182cce894
fffxue <18897960357@163.com>
2019-10-25 10:11:57 +0800

    email test

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,23 +1,57 @@
-import BetterRegExp from '../src/index'
+import betterRegExp from '../src/index'
 
+const bRE = new betterRegExp()
 describe("正则表达式", () => {
-    test("测试18897960357是手机号码", () => {
-        const BRE = new BetterRegExp()
-        expect(BRE.isPhoneNumber(18897960357)).toBeTruthy()
+
+    describe("测试手机号码", () => {
+        test("18897962257是手机号码", () => {
+            expect(bRE.isPhone(18897960357)).toBeTruthy()
+        })
+    
+        test("14894560357是手机号码", () => {
+            expect(bRE.isPhone(14897560357)).toBeTruthy()
+        })
+    
+        test("12897569957不是手机号码", () => {
+            expect(bRE.isPhone(12897569957)).toBeFalsy()
+        })
+    
+        test("177897563357不是手机号码", () => {
+            expect(bRE.isPhone(177897563357)).toBeFalsy()
+        })
+
+        test("1778973357不是手机号码", () => {
+            expect(bRE.isPhone(1778973357)).toBeFalsy()
+        })
     })
 
-    test("测试14897560357是手机号码", () => {
-        const BRE = new BetterRegExp()
-        expect(BRE.isPhoneNumber(14897560357)).toBeTruthy()
-    })
+    describe("测试邮箱", () => {
+        test("794333@qq.com是邮箱", () => {
+            expect(bRE.isEmail("794333@qq.com")).toBeTruthy()
+        })
 
-    test("测试12897560357不是手机号码", () => {
-        const BRE = new BetterRegExp()
-        expect(BRE.isPhoneNumber(12897560357)).toBeFalsy()
-    })
+        test("710578897@qq.com是邮箱", () => {
+            expect(bRE.isEmail("710578897@qq.com")).toBeTruthy()
+        })
 
-    test("测试177897560357不是手机号码", () => {
-        const BRE = new BetterRegExp()
-        expect(BRE.isPhoneNumber(177897560357)).toBeFalsy()
+        test("100578897@qq.com是邮箱", () => {
+            expect(bRE.isEmail("100578897@qq.com")).toBeTruthy()
+        })
+
+        test("7105722337@163.com是邮箱", () => {
+            expect(bRE.isEmail("7105722337@163.com")).toBeTruthy()
+        })
+
+        test("7105722337@163.com是邮箱", () => {
+            expect(bRE.isEmail("7105722337@163.com")).toBeTruthy()
+        })
+
+        test("liu_jian_feng-196-12-14@gmail.com是邮箱", () => {
+            expect(bRE.isEmail("liu_jian_feng-196-12-14@gmail.com")).toBeTruthy()
+        })
+
+        test("105*9989@gmail.com不是邮箱", () => {
+            expect(bRE.isEmail("105*9989@gmail.com")).toBeFalsy()
+        })
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 
 export default class BetterRegExp{
+
+    public phoneReg = /^1[3456789]\d{9}\b/
+
+    // 允许英文字母、数字、下划线、英文句号、以及中划线组成
+    public emainReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)$/
+
     constructor(){ 
 
     }
 
-    isPhoneNumber (inputStr:number) : Boolean{
-        if(/^1[3456789]\d{9}\b/.test(inputStr.toString())){
+    isPhone (inputNum:number) : Boolean{
+        if(this.phoneReg.test(inputNum.toString())){
             return true
         }
         return false
     }
 
-    isEmailNumber (inputStr:String) : Boolean{
+    isEmail (inputStr:any) : Boolean{
+        if(this.emainReg.test(inputStr.toString())){
+            return true
+        }
         return false
     }
 }
